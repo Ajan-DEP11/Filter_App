e10dd9141e6682b4204159de11dc623d8db540d4
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-11-26 10:36:01 +0530

    Add test cases

---
+++ src/test/java/lk/ijse/dep11/DatabaseTest.java
@@ -0,0 +1,26 @@
+package lk.ijse.dep11;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DatabaseTest {
+    @Test
+    public void testDBExceeds1000records(){
+
+    }
+
+    @Test
+     public void testValidContacts(){
+
+    }
+
+    @Test
+    public void testUniqueContacts(){
+
+    }
+
+    @Test
+    public void testUniqueCustomerNames(){
+
+    }
+}
